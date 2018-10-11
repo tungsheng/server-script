1c4102a3b91724370e6d91e32c2243ef588a2b16
Tony Lee <tungsheng@gmail.com>
2018-10-11 13:45:47 +0800

    add firewall

--- init.sh
+++ init.sh
@@ -2,21 +2,26 @@
 export log_file=$HOME/install_progress_log.txt
 export repo=$HOME/server-script
 
-echo -ne "Disable root user...\n"
+println() {
+    echo -ne "\n$1"
+    echo "======================\n"
+}
+
+println "Disable root user..."
 sudo passwd -l root
 
-echo -ne "Add user...\n"
+println "Add user..."
 adduser deploy
 
-echo -ne "Add user to sudo...\n"
+println "Add user to sudo..."
 usermod -aG sudo deploy
+cp -r ~/.ssh /home/sammy
+chown -R sammy:sammy /home/sammy/.ssh
 
-source "./yespasswd.sh"
-
-echo -ne "Initiating...\n"
+println "Initiating..."
 sudo apt -y update
 
-echo -ne "Installing utils...\n"
+println "Installing utils..."
 sudo apt -y install \
     apt-transport-https \
     autoconf \
@@ -35,6 +40,7 @@ sudo apt -y install \
     libunibilium0 \
     locate \
     make \
+    man-db \
     openssh-server \
     pkg-config \
     python-pip \
@@ -42,29 +48,35 @@ sudo apt -y install \
     silversearcher-ag \
     software-properties-common \
     tig \
+    ufw \
     whois \
     wget
 
-echo -ne "Installing nvm...\n"
+println "Enable firewall..."
+ufw allow OpenSSH
+ufw enable
+ufw status
+
+println "Installing nvm..."
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 command -v nvm
 
-echo -ne "Installing node...\n"
+println "Installing node..."
 nvm install node
 
-echo -ne "Installing yarn...\n"
+println "Installing yarn..."
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update && sudo apt install yarn
 
-echo -ne "Installing fasd...\n"
+println "Installing fasd..."
 wget -c https://github.com/clvv/fasd/tarball/1.0.1 -O - | tar -xz
 cd clvv-fasd-4822024/
 make install
 cd ../
 rm -rf clvv-fasd-4822024/
 
-echo -ne "Installing bash-it...\n"
+println "Installing bash-it..."
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 ~/.bash_it/install.sh
 source /root/.bashrc
@@ -80,21 +92,20 @@ cp $repo/aliases/custom.aliases.bash $HOME/.bash_it/aliases/
 # use bakke theme
 sed -i 's/bobby/bakke/g' .bashrc
 
-echo -ne "Installing neovim...\n"
+println "Installing neovim..."
 curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
 chmod u+x nvim.appimage
 pip2 install neovim
 pip3 install neovim
 
-echo "Install color...\n"
+println "Install color..."
 tic -x $repo/color/xterm-256color-italic.terminfo
 tic -x $repo/color/tmux-256color-italic.terminfo
 
-echo "Update packages...\n"
+println "Update packages..."
 sudo apt -y update
 
-echo -e "\n\ninstalling to ~/.config"
-echo "=============================="
+println "installing to ~/.config"
 if [ ! -d $HOME/.config ]; then
     echo "Creating ~/.config"
     mkdir -p $HOME/.config
@@ -109,4 +120,3 @@ for config in $repo/config/*; do
         ln -s $config $target
     fi
 done
-
