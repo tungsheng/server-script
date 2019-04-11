178e6fdd9c5a3a405a8c665a4daef2e2324e14d9
Tony Lee <tungsheng@gmail.com>
2019-04-12 02:09:53 +0800

    update docker composer version

--- docker.sh
+++ docker.sh
@@ -15,7 +15,7 @@ apt-cache policy docker-ce
 sudo apt install -y docker-ce
 
 # install docker compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # show docker compose version
@@ -27,4 +27,3 @@ println "Add user to docker group"
 usermod -aG docker deploy
 
 println "docker successfully installed!!"
-
